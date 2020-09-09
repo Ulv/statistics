39b6acfe677b197e3b6d854c90d26ea5d141c7dc
Volodymyr Chmil <chmil@smartproject.ua>
2020-09-09 15:55:03 +0300

    Updated examples.php

--- examples.php
+++ examples.php
@@ -4,14 +4,13 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 $array = range(4, 91, 3);
 \shuffle($array);
-$weights = range(1, count($array));
 
 $distribution = new \Ulv\Statistics\Distribution();
 
 var_dump($distribution->mean($array)); // 47.5
 var_dump($distribution->median($array)); // 47.5
 var_dump($distribution->mode($array)); // 4
-var_dump($distribution->weightedMean($array, $weights)); // ...
+var_dump($distribution->weightedMean([1, 4, 16, 2, 6, 3], [10, 20, 15, 4, 32, 11])); // 6.1195652173913
 
 $variation = new \Ulv\Statistics\Variation();
 
